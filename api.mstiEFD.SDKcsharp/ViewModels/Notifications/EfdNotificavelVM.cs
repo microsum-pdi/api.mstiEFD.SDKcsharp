ef283e751e1ecc8f3489e0f77566f1e5b4c48266
silvairls <silvairls@microsum.com.br>
2023-09-28 11:58:23 -0300

    Ajustes para remover as propriedades do Flunt dos retornos das requisições.

--- api.mstiEFD.SDKcsharp/ViewModels/Notifications/EfdNotificavelVM.cs
+++ api.mstiEFD.SDKcsharp/ViewModels/Notifications/EfdNotificavelVM.cs
@@ -1,11 +1,13 @@
 ﻿using Flunt.Notifications;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NJsonSchema.Annotations;
 using System.Runtime.Serialization;
 
 namespace api.mstiEFD.SDKcsharp.ViewModels.Notifications
 {
     [ApiExplorerSettings(IgnoreApi = true)]
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class EfdNotificavelVM :  Notifiable<Notification>
     {
         [IgnoreDataMember]
@@ -16,10 +18,15 @@ namespace api.mstiEFD.SDKcsharp.ViewModels.Notifications
         [JsonIgnore]
         public bool Valid { get { return !Invalid; } }
 
+        [IgnoreDataMember]
+        [JsonIgnore]
+        [JsonSchemaIgnore]
+        public IReadOnlyCollection<Notification> Notifications => base.Notifications;
 
         [IgnoreDataMember]
         [JsonIgnore]
-        public IReadOnlyCollection<Notification> Notifications => base.Notifications;
+        [JsonSchemaIgnore]
+        public IReadOnlyCollection<Notification> Isvalid => base.Notifications;
 
         /// <summary>
         /// Retorna a lista de falhas de validação (propriedade "Notifications"), em uma lista de string
